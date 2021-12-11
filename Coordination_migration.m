d81e5fe2b8c296d86ea90d731fb5ae92a3039ec9
Minhua Yan <32174252+Minhua-Yan@users.noreply.github.com>
2021-12-10 21:08:19 -0700

    Add files via upload

--- Coordination_migration.m
+++ Coordination_migration.m
@@ -15,14 +15,15 @@
 %     z: norm (mean of the trait distribution) of the source population
 %     U_sd: standard deviation of the source population's trait distribution
 %     f: initial norm (mean of the initial trait distribution) of the focal population
-%     S: strength of social selection as pairwise coordination in the focal population
+%     S_inv: strength of social selection as pairwise coordination in the focal population
+%        S_inv is the inverse of S in the base model in the main text
 %     E_sd: standard deviation of the error in social learning
 
 %% ----------------------------------------------------------------- assign values to parameters
 
-m = 0.0000000;               
-z = 3;      f = 0;                                                                
-U_sd = 2;   S = 2;    E_sd = 1;
+m = 0.1;               
+z = 6.12;      f = 0;                                                                
+U_sd = 0.001;   S_inv = 1;    E_sd = 1;
 
 
 
@@ -31,6 +32,7 @@ U_sd = 2;   S = 2;    E_sd = 1;
 % since we cannot maintain normality, we solve for the distribution analytically by approxiamating the distribution of the norm trait and of the error, with bins
 
 binRange= 30;                                                                % the trait range on one side of f we keep track of
+% reminder: this may need to be changed depending on E, S and z
 binWid = 0.01;                                                               % the width of the bins we cut the distributions into
 nbins = 2*round( binRange /binWid ) +1;                                      % make the total number of bins an odd number, so the center of the center bin is exactly "f"
 
@@ -43,20 +45,20 @@ xM = xU - binWid/2;                                                          % x
 
 
 %% initialize focal population N(f,V)
-V = E_sd^2 + sqrt(E_sd^4+E_sd^2/S);                                          % equilibrium variance with only transission error and norm selection, see "eq V no mig calculation.pdf"
+V = E_sd^2 + sqrt(E_sd^4+E_sd^2/S_inv);                                      % equilibrium variance with only 1-parent social learning, transission error N~(0,E) and norm selection with strength S_inv, see "eq V with social selection and SL error.pdf"
 V_sd = sqrt(V);                                                              % equilibrium standard deviation
-iFreq = normcdf(xU,f,V_sd) - normcdf(xL,f,V_sd);                             % a row, density of the initial island norm traits; the ith element gives the frequency of the ith bin
-iFreq = iFreq/sum(iFreq);
+fFreq = normcdf(xU,f,V_sd) - normcdf(xL,f,V_sd);                             % a row, density of the initial island norm traits; the ith element gives the frequency of the ith bin
+fFreq = fFreq/sum(fFreq);
 
 
-%% initialize source population N(c,U)
+%% initialize source population N(z,U)
 zFreq = normcdf(xU,z,U_sd) - normcdf(xL,z,U_sd);                             % a row, density of the continent trait values between [minX +(i-1)*binWid, minX +i*binWid]
 zFreq = zFreq/sum(zFreq);
 
 
 %% initialize copying error, a normal distribution with mean == 0 and standard deviation E_sd
 % error is also approxiamited with bins of width "binWid"            
-errRange = 10*E_sd;                                                           % the error range on one side of 0 we keep track of
+errRange = 6*E_sd;                                                           % the error range on one side of 0 we keep track of
 nEbins = 2* round( errRange/binWid ) + 1;                                     % number of error bins, odd so the center of the center bin is exactly 0
 minErrRange = -nEbins/2*binWid;                                               % updated lower boundary of the error range
 xU_Err = ((1:nEbins)*binWid + minErrRange);                                   % upper bounds of the error bins
@@ -92,119 +94,157 @@ end
    
 %% prepare matrices to contain information through the evolutionary process (for plotting)
 
-gen = 30000;                                                                     % the number of generations to run the evolution for
+gen = 500;                                                                     % the number of generations to run the evolution for
 
 pdf_Mig_Gen = zeros(gen, nbins);                                             % gth row: trait distribution in generation g after migration
 pdf_SS_Gen = zeros(gen, nbins);                                              % gth row: trait distribution in generation g after social selection (SS)
 pdf_SL_Gen = zeros(gen, nbins);                                              % gth row: trait distribution in generation g after social learning (SL) w/o error
 pdf_Err_Gen = zeros(gen, nbins);                                             % gth row: trait distribution in generation g after SL error
 
-Fit_Gen = zeros(gen,nbins);                                                  % gth row: fitness values for xM in social selection in generation g
-Mean_Gen = zeros(gen, 3);                                                    % 1 column: mean after Mig, 2nd column: mean after SS, 3rd column: mean after SL with err
+fit_Soc_Gen = zeros(gen,nbins);                                                  % gth row: fitness values for xM in social selection in generation g
+Mean_Gen = zeros(gen, 4);                                                    % 1 column: mean after Mig, 2nd column: mean after SS, 3rd column: mean after SL with err
 
 
 %% focal population distribution's evolutionary process
 
 for g = 2:gen
 
-  % migration
-    iFreq = (1-m).*iFreq + m.*zFreq;    
-    iFreq = iFreq/sum(iFreq);
-    pdf_Mig_Gen(g,:) = iFreq; 
-    Mean_Gen(g,1) = sum(iFreq.*xM);
+  %% migration
+    fFreq = (1-m).*fFreq + m.*zFreq;    
+    fFreq = fFreq/sum(fFreq);
+    pdf_Mig_Gen(g,:) = fFreq; 
+    Mean_Gen(g,1) = sum(fFreq.*xM);
     
 
-  % social selection as coordination
-    fit = zeros(1,nbins);
+  %% social selection as coordination
+    fit_Soc = zeros(1,nbins);
     for i = 1: nbins
-        fit(i) = sum(exp(-S*dis(i,:).*dis(i,:)/2).*iFreq);  
+        fit_Soc(i) = sum(exp(-S_inv *dis(i,:).*dis(i,:)/2).*fFreq);  
     end
-    Fit_Gen(g,:) = fit;         
+    fit_Soc_Gen(g,:) = fit_Soc;         
     
-    iFreq = iFreq.*fit;
-    iFreq = iFreq/sum(iFreq);
-    pdf_SS_Gen (g,:) = iFreq;
-    Mean_Gen(g,2) = sum(iFreq.*xM);
- 
+    fFreq = fFreq.*fit_Soc;
+    fFreq = fFreq/sum(fFreq);
+    pdf_SS_Gen (g,:) = fFreq;
+    Mean_Gen(g,2) = sum(fFreq.*xM);
+  
+  %% social learning without error
+  % trying to learn the norm of one cultural parent
+    fFreq = fFreq;
+    Mean_Gen(g,3) = sum(fFreq.*xM);
     
 %   % trying to learn the mean of two cultural parents
-%     iCombM = transpose(iFreq)*iFreq;                                        %a matrix containing probabilities of different combinations                                                 %the number of averages of combinations
-%     iCombMean = zeros(1,2*nbins-1);                                         %a rwo containing the probabilities of different averages of combinations
+%     fCombM = transpose(fFreq)*fFreq;                                        %a matrix containing probabilities of different combinations                                                 %the number of averages of combinations
+%     fCombMean = zeros(1,2*nbins-1);                                         %a rwo containing the probabilities of different averages of combinations
 %     for i = 1:nbins
 %         for j = 1:i
-%            iCombMean(1,i) = iCombMean(1,i) +iCombM(j,i+1-j); 
+%            fCombMean(1,i) = fCombMean(1,i) +fCombM(j,i+1-j); 
 %         end
 %     end
 %     for i = nbins+1 : 2*nbins-1
 %         for j = i+1-nbins :nbins
-%             iCombMean(1,i) = iCombMean(1,i) +iCombM(j,i+1-j);
+%             fCombMean(1,i) = fCombMean(1,i) +fCombM(j,i+1-j);
 %         end
 %     end
-%     iFreq = iCombMean(1:2:end) + [1/2*iCombMean(2:2:end),0]+[0,1/2*iCombMean(2:2:end)];   
-%     Dis_SL_Gen(t,:)=iFreq; 
-%     Mean_Gen(g,3) = sum(iFreq.*xM);    
+%     fFreq = fCombMean(1:2:end) + [1/2*fCombMean(2:2:end),0]+[0,1/2*fCombMean(2:2:end)];   
+%     pdf_SL_Gen(g,:)=fFreq; 
+%     Mean_Gen(g,3) = sum(fFreq.*xM);    
       
     
     
     % social learning error
-    iFreq = iFreq*Trans;
-    iFreq = iFreq/sum(iFreq);
-    pdf_Err_Gen(g,:) = iFreq;
-    Mean_Gen(g,3) = sum(iFreq.*xM);                                          
+    fFreq = fFreq*Trans;
+    fFreq = fFreq/sum(fFreq);
+    pdf_Err_Gen(g,:) = fFreq;
+    Mean_Gen(g,4) = sum(fFreq.*xM);
+    
+    [Mean_Gen(g-1,4),Mean_Gen(g,:)]
     
 end
 
+eq_cdf = zeros(1,nbins+1);
+for loc = 1:nbins
+    eq_cdf(loc+1) = sum(fFreq(1:loc));
+     if eq_cdf(loc+1) >1
+        eq_cdf(loc+1) =1;
+     end
+end
+    eq_cdf=[eq_cdf,1];
+    dist_x = [xM(1)-binWid,xM,xM(end)+binWid];
+    
+    for pdf_pos = 1:length(eq_cdf)-1
+        if eq_cdf(pdf_pos) > eq_cdf(pdf_pos+1)
+           msg2 = 'Error occurred: eq_cdf is not a cdf';
+           error(msg2)
+        end
+    end
+    pd = makedist('PieceWiselinear','X',dist_x,'FX',eq_cdf);
+    sd = sqrt(var(pd));
+    
+
 %% figure 1: the norm trait distribution after different stages (migration, social selection, social learning w/ error) in one generation 
 figure; hold on;
-g_plot=2;                 % the generation we plot the distributions for
+g_plot=9;                 % the generation we plot the distributions for
 plot(xM, pdf_Mig_Gen(g_plot,:), xM, pdf_SS_Gen(g_plot,:),  xM, pdf_Err_Gen(g_plot,:),  'LineWidth',1);
-legend('after migration','after social selection','after social learning w/ error')
-xlabel('trait value');
-ylabel('density');
+legend('After migration','After social selection','After social learning with error','FontSize',12)
+xlabel('Norm trait value');
+ylabel('Distribution density');
 xlim([-10 10])
-title(['norm trait distribution after different stages in generation ', num2str(g_plot)]);
+%title(['norm trait distribution after different stages in generation ', num2str(g_plot)]);
 ax = gca;
 ax.FontSize = 16;
 
 %% figure 2: the norm evolutionary trajectory
 figure; hold on;
 plot(1:gen, Mean_Gen(:,3),'LineWidth',2)
-xlabel('generation')
-ylabel('mean of norm trait distribution')
-line([0,300],[z,z],'color','black','LineWidth',1)
-title('norm evolution trajectory in the focal population')
+xlabel('Generation')
+ylabel('Mean norm value')
+line([0,gen],[z,z],'color','black','LineWidth',1)
+%title('norm evolution trajectory in the focal population')
 ax = gca;
 ax.FontSize = 16;
 
 
+%% figure 3: the norm evolution in each stage
+Mean_each_step = zeros(1,4*g);
+for g = 1:gen
+    Mean_each_step(1,4*g-3)=Mean_Gen(g,1);     %after migration
+    Mean_each_step(1,4*g-2)=Mean_Gen(g,2);     %after social selection
+    Mean_each_step(1,4*g-1)=Mean_Gen(g,3);     %after social learning w/o error
+    Mean_each_step(1,4*g)=Mean_Gen(g,4);         %after social learning error
+end
 
-% Mean_each_step = zeros(1,3*g);
-% for g = 1:gen
-%     Mean_each_step(1,3*g-2)=Mean_Gen(g,1);     %after migration
-%     Mean_each_step(1,3*g-1)=Mean_Gen(g,2);     %after social selection
-%     Mean_each_step(1,3*g)=Mean_Gen(g,3);       %after social learning with error
-% end
-% 
-% figure;
-% hold on;
-% plot(1:3*gen,  Mean_each_step,   'lineWidth',0.01)
-% xlabel('generation')
-% ylabel('mean of norm trait distribution')
-% title('norm evolution trajectory in the focal population')
+figure;
+hold on;
+plot(1:4*gen,  Mean_each_step,   'lineWidth',0.01)
+xlabel('generation')
+ylabel('mean of norm trait distribution')
+title('norm evolution trajectory in the focal population given the stages')
 
 
-% figure;
-% hold on;
-% [ax, h1, h2] = plotyy(xM(500:2502), iNSpostMig(2,500:2502),xM(500:2502),iNSfit(2,500:2502));
-% h1.LineStyle = '--';
-% h1.LineWidth = 2;
-% h2.LineWidth = 2;
-% xlim([-10 10])
-% ylim(ax(1),[0 0.007]);
-% ylim(ax(2),[0 0.8]);
-% line([iNSMean(2,2),iNSMean(2,2)],[0,0.2],'color','black','LineWidth',1.5)
-% legend('population distribution','population mean','social selection fitness function')
-% x3=xlabel('trait value','Position',[0 -0.00045 -1]);
-% h= title('social selection fitness function for a population distribution');
-% ax = gca;
-% ax.FontSize = 16;
+%% figure 4: population distribution after migration and social selection fitness function
+figure;
+hold on;
+g_plot=150;                 % the generation we plot the distribution and fitness function for
+
+yyaxis left
+plot(xM,fit_Soc_Gen(g_plot,:),'Color',[0.8500, 0.3250, 0.0980]);
+ylabel('fitness','FontSize',20);
+
+yyaxis right
+plot(xM, pdf_Mig_Gen(g_plot,:),'Color','black');
+ylim([0 0.005])
+ylabel('density','FontSize',20);
+
+line([Mean_Gen(g_plot,1),Mean_Gen(g_plot,1)],[0,0.2],'color','black','LineWidth',1.5)    % after migration distribution mean
+
+%xlim([-10 10])
+
+leg = legend('social selection fitness function','norm distribution density function','FontSize',12);
+title('social selection fitness function for a population distribution');
+ax = gca;   ax.FontSize = 16;
+ax.YAxis(1).Color = [0.8500, 0.3250, 0.0980];
+ax.YAxis(2).Color = 'black';
+xlabel('trait value','FontSize',20);
+
+
